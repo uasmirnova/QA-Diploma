c98814d728f534726c19881bec605082e07eebe8
uasmirnova <uasmirnova21@yandex.ru>
2022-12-17 21:14:48 +0300

    pages added

---
+++ src/test/java/ru/netology/tests/PaymentFormTest.java
@@ -0,0 +1,55 @@
+package ru.netology.tests;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.DbHelper;
+import ru.netology.pages.PaymentPage;
+import ru.netology.pages.StartPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DbHelper.cleanDatabase;
+
+public class PaymentFormTest {
+
+    @BeforeAll
+    public static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:8080/");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        cleanDatabase();
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @Epic(value = "Тестирование UI")
+    @Feature(value = "Оплата тура с карты")
+    @Story(value = "Позитивный. Покупка тура с карты (с пробелами)")
+    @Test
+    public void shouldValidTestCardApproved() {
+        StartPage startPage = new StartPage();
+        var CardInfo = DataHelper.getValidCardApproved();
+        PaymentPage paymentPage = startPage.paymentButtonClick();
+        paymentPage.inputData(CardInfo);
+        paymentPage.getSuccessNotification();
+        //assertEquals("APPROVED", DbHelper.getPaymentStatus());
+    }
+}
